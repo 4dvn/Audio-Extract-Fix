09f4b1788d83fa87051848522f4edad710a803cd
Elad Karako <eladkarako@users.noreply.github.com>
2017-08-13 00:50:54 +0300

    initial

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Audio-Extract-Fix
+<h1>Audio-Extract-Fix</h1>
+
 [FFMPEG] [EXIFTOOL] Audio Recover And Fix. + Drag&amp;Drop Support. Will Extract, Recover, Fix, Clean-Up And Export-Out A Valid, Usable Audio Stream. Works With Almost Any Files. Default Output Format: MP3 (Can Be Set To Anything: oga, m4a, ...)
+
+the <code>ffmpeg.exe</code> and <code>exiftool.exe</code> are compressed, extract the exe to the main folder on first usage.
---
+++ audio_fix.cmd
@@ -0,0 +1,50 @@
+@echo off
+
+
+set FILE_INPUT="%~1"
+
+
+echo.
+echo Removing Meta-Data (From %FILE_INPUT%)
+@echo on
+call exiftool.exe -progress -verbose -trailer:all="" -XMPToolkit="" -all="" %FILE_INPUT%
+@echo off
+echo.
+echo Done.
+
+
+set ARGS=
+::program switches
+set ARGS=%ARGS%  -y  -hide_banner  -loglevel "info"  -stats  -threads "16"  -strict "experimental"
+::flags
+  set ARGS=%ARGS% -flags              "+naq+low_delay+global_header-unaligned-ilme-cgop-loop-output_corrupt"
+  set ARGS=%ARGS% -flags2             "+fast+ignorecrop+showall+export_mvs"
+  set ARGS=%ARGS% -fflags             "+ignidx+genpts+nofillin+discardcorrupt-fastseek"
+  set ARGS=%ARGS% -movflags           "+faststart+disable_chpl"
+  set ARGS=%ARGS% -avoid_negative_ts  "make_zero"
+::start/finish
+  set ARGS=%ARGS% -ss "00:00:00.001"
+::set ARGS=%ARGS% -to "00:00:05.000"
+::meta data processing
+  set ARGS=%ARGS% -map_chapters  "-1"
+  set ARGS=%ARGS% -map_metadata  "-1"
+::explicit "audio only" mode
+  set ARGS=%ARGS% -sn -vn
+::audio processing
+  set ARGS=%ARGS% -af "asetpts=PTS-STARTPTS,aresample=async=1:min_hard_comp=0.100000" -vsync "1"
+::audio quality
+  set ARGS=%ARGS% -b:a 128k -ar 44100
+
+
+set FILE_OUTPUT="%~d1%~p1%~n1_fixed.mp3"
+
+echo.
+echo Encoding Audio Stream.
+@echo on
+call ffmpeg.exe -i %FILE_INPUT% %ARGS% %FILE_OUTPUT%
+@echo off
+echo.
+echo Done.
+
+
+pause
